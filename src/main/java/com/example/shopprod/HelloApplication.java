d3878531421866739f9a8d230cde75ccfb9e204e
Cverlllly <anej.cverle@scv.si>
2023-04-21 11:37:04 +0100

    Login/register set up

--- src/main/java/com/example/shopprod/HelloApplication.java
+++ src/main/java/com/example/shopprod/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.shopprod;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +11,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Login");
+        Parent root= FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Scene scene=new Scene(root);
+        stage.setTitle("Shopify");
+        stage.setMinHeight(550);
+        stage.setMinWidth(850);
+
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/shopprod/LoginController.java
+++ src/main/java/com/example/shopprod/LoginController.java
@@ -1,16 +1,20 @@
 package com.example.shopprod;
 
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.awt.event.ActionEvent;
 
 public class LoginController {
 
     @FXML
-    private Hyperlink already_account;
+    private Button already_account;
 
     @FXML
     private Button create_account;
@@ -51,4 +55,37 @@ public class LoginController {
     @FXML
     private AnchorPane side_pane;
 
+
+    public void SwitchForm(javafx.event.ActionEvent event) {
+        TranslateTransition slider=new TranslateTransition();
+        if(event.getSource()==create_account){
+            slider.setNode(side_pane);
+            slider.setToX(400);
+            slider.setToZ(20);
+            slider.setDuration(Duration.seconds(.5));
+            slider.setOnFinished((actionEvent) -> {
+                register_form.setVisible(true);
+                login_form.setVisible(false);
+                create_account.setVisible(false);
+                already_account.setVisible(true);
+            });
+            slider.play();
+        }
+        else if(event.getSource()==already_account){
+            slider.setNode(side_pane);
+            slider.setToX(0);
+            slider.setToZ(20);
+            slider.setDuration(Duration.seconds(.5));
+            slider.setOnFinished((actionEvent) -> {
+                register_form.setVisible(false);
+                login_form.setVisible(true);
+                create_account.setVisible(true);
+                already_account.setVisible(false);
+
+            });
+            slider.play();
+        }
+        /*-----------------------------------------------------------------------------------------------------------------------*/
+
+    }
 }
