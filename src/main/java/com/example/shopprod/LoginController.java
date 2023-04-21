ec53f9734e89c3718820c7c61e0814cd8f9f749e
Cverlllly <anej.cverle@scv.si>
2023-04-21 11:37:04 +0100

    Login/register set up

---
+++ src/main/java/com/example/shopprod/LoginController.java
@@ -0,0 +1,54 @@
+package com.example.shopprod;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+
+    @FXML
+    private Hyperlink already_account;
+
+    @FXML
+    private Button create_account;
+
+    @FXML
+    private PasswordField log_password;
+
+    @FXML
+    private TextField log_username;
+
+    @FXML
+    private Button login_button;
+
+    @FXML
+    private AnchorPane login_form;
+
+    @FXML
+    private Hyperlink no_password;
+
+    @FXML
+    private TextField reg_email;
+
+    @FXML
+    private PasswordField reg_password;
+
+    @FXML
+    private PasswordField reg_re_passowrd;
+
+    @FXML
+    private Button reg_signup;
+
+    @FXML
+    private TextField reg_username;
+
+    @FXML
+    private AnchorPane register_form;
+
+    @FXML
+    private AnchorPane side_pane;
+
+}
