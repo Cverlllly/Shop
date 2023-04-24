331ab05accf122ff3c02512fb6f2ab55b14def17
Cverlllly <anej.cverle@scv.si>
2023-04-24 20:11:54 +0100

    all done/ WIP displaying items

--- src/main/java/com/example/shopprod/UsersPanelController.java
+++ src/main/java/com/example/shopprod/UsersPanelController.java
@@ -1,4 +1,22 @@
 package com.example.shopprod;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class UsersPanelController {
+
+    @FXML
+    private Button cart;
+
+    @FXML
+    private AnchorPane displayitems;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private Button search_btn;
+
 }
